1fba8a2e5de94098707aad1c07013bd7e5ed091d
mclacs14 <bradleyg36@gmail.com>
2015-01-18 20:51:44 -0500

    T.T

--- WebPort/app/controllers/users_controller.rb
+++ WebPort/app/controllers/users_controller.rb
@@ -10,10 +10,10 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
-    flash[:success] = "Welcome to WebPort!"
-     redirect_to @user
+    	flash[:success] = "Welcome to WebPort!"
+      redirect_to @user
     else
       render 'new'
     end
@@ -24,6 +24,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-
     end
 end
