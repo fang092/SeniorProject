608936372f78c56d7e36e1e16dcaac0f269d352d
mclacs14 <bradleyg36@gmail.com>
2015-03-13 22:48:52 -0400

    Trying to fix login path

--- WebPort/app/controllers/sessions_controller.rb
+++ WebPort/app/controllers/sessions_controller.rb
@@ -7,9 +7,10 @@ class SessionsController < ApplicationController::Base
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
-      log_in user
+      log_in @user
+      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
-
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
@@ -17,6 +18,8 @@ class SessionsController < ApplicationController::Base
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 
 end
