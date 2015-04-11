8dc02062a54b4ebc1a02184ec546fed29ba17f0d
mclacs14 <bradleyg36@gmail.com>
2015-04-10 23:04:22 -0400

    At Activating Account

--- WebPort/app/models/user.rb
+++ WebPort/app/models/user.rb
@@ -11,6 +11,9 @@ class User < ActiveRecord::Base
                     has_secure_password
                     validates :password, length: { minimum: 6 }, allow_blank: true
 
+  has_attached_file  :image, styles: { large: "600x600#" , medium: "300x300#", thumb: "100x100#" }
+  validates_attachment_content_type :image, content_type:  /\Aimage\/.*\Z/
+
  # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
--- WebPort/db/schema.rb
+++ WebPort/db/schema.rb
@@ -11,19 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404181534) do
+ActiveRecord::Schema.define(version: 20150411020350) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin",             default: false
+    t.boolean  "admin",              default: false
     t.string   "activation_digest"
-    t.boolean  "activated",         default: false
+    t.boolean  "activated",          default: false
     t.datetime "activated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
