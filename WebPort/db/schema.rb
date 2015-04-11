604cc1f88ea97dd483e96185afe87ed69981a44e
mclacs14 <bradleyg36@gmail.com>
2015-04-11 15:29:15 -0400

    Added Password resets and email configuration

--- WebPort/db/schema.rb
+++ WebPort/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411020350) do
+ActiveRecord::Schema.define(version: 20150411183246) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20150411020350) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
